c05a7adb17077a1e15c7d846ea2c251a3344a642
FDzerzhinsky <93994427+FDzerzhinsky@users.noreply.github.com>
2022-11-15 23:09:37 +0500

    Sample text

---
+++ sumOfFactorials.py
@@ -0,0 +1,15 @@
+print('Введите n:')
+n = int(input())
+def fact(n):
+    if n == 1:
+        return 1
+    else:
+        return(n*fact(n-1))
+
+def sumFact(a):
+    if a == 0:
+        return 0
+    else:
+        return sumFact(a-1) + fact(a)
+        
+print(f'Сумма факториалов от 1 до n: {sumFact(n)}')
